b65dec96f23410686d69fa84b25394e570b12f22
Bogdan Tatarov <bogdan@tatarov.me>
2024-01-02 14:26:53 +0800

    windows (build): set VS version to 2022

--- bin/build-windows.bat
+++ bin/build-windows.bat
@@ -1,30 +1,20 @@
 @echo off
-rem :: Determine target directory and cmake generator
 setlocal enableextensions
+
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
 
 where lib || echo "Could not find lib.exe (are you in your VS developer tools prompt?)" && exit /b 1
 
-:haslib
-set arg1=%1
-if "%arg1%"=="" set arg1=vs2015
-set generator=
-if "%arg1%"=="vs2008" set generator=Visual Studio 9 2008
-if "%arg1%"=="vs2010" set generator=Visual Studio 10
-if "%arg1%"=="vs2012" set generator=Visual Studio 11
-if "%arg1%"=="vs2013" set generator=Visual Studio 12
-if "%arg1%"=="vs2015" set generator=Visual Studio 14
-if "%generator%"=="" (
-	@echo Unknown argument "%1". Valid values are vs2008, vs2010, vs2012, vs2013, vs2015. Exiting.
-	exit /b 1
-)
+set generator=Visual Studio 17
+
 cd %~dp0%..
 set rootpath=%cd%
-set defaultprefix=%rootpath%\lib\windows\%arg1%
+set defaultprefix=%rootpath%\lib\windows\vs2022
 set libprefix=%2
 if "%libprefix%"=="" set libprefix=%defaultprefix%
 
-mkdir "build\build-%arg1%"
-cd "build\build-%arg1%"
+mkdir "build\build-vs2022"
+cd "build\build-vs2022"
 
 echo Creating Release Libs
 cmake -G "%generator%" ^
@@ -37,13 +27,12 @@ cmake -G "%generator%" ^
 -DCMAKE_INSTALL_PREFIX=%libprefix%\Release ^
 %rootpath%\cmake\hosts\host-win-sdl || exit /b 1
 
-cmake --build . --target INSTALL --config Release || exit /b 1
+cmake --build . --target INSTALL --config Release -- -maxcpucount:12 || exit /b 1
 
-erase  libmoai\third-party\luajit\luajit\src\lua51.lib
+erase libmoai\third-party\luajit\luajit\src\lua51.lib
 
 rem echo Creating Debug Libs
 rem cmake -DCMAKE_INSTALL_PREFIX=%libprefix%\Debug %rootpath%\cmake\hosts\host-win-sdl || exit /b 1
-
 
 rem if "%CI%"=="TRUE" goto skipdebug
 rem cmake --build . --target INSTALL --config Debug  || exit /b 1
@@ -59,9 +48,6 @@ rem lib /OUT:%libprefix%\Distribute\lib\moai_d.LIB %libprefix%\Debug\lib\*.lib |
 xcopy /S/I/Y %libprefix%\Release\include %libprefix%\Distribute\include  || exit /b 1
 mkdir %libprefix%\Distribute\bin
 copy /Y %libprefix%\Release\bin\moai.exe %libprefix%\Distribute\bin\moai.exe
-
-
-if NOT EXIST %rootpath%\util\moai.exe copy /Y %libprefix%\Release\bin\moai.exe %rootpath%\util\moai.exe
 
 rd /S/Q %libprefix%\Release
 rem rd /S/Q %libprefix%\Debug
